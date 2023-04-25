456a08f8dc4357077a7a8a35a88a822412dbdf54
Albert Waz <albewaz@gmail.com>
2023-04-25 20:37:24 +0200

    function init adding

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-const usunNaglowek = () => {
-  
-    const przycisk = document.querySelector(".button");
-    const naglowek = document.querySelector(".header");
-  
-    przycisk.addEventListener("click", () => {
-      naglowek.remove();
-    });
-  
-  }
-  
-  usunNaglowek();
+{
+const init = () => {
+  removeHeader();
+}
+
+const removeHeader = () => {
+  const przycisk = document.querySelector(".button");
+  const naglowek = document.querySelector(".header");
+
+  przycisk.addEventListener("click", () => {
+    naglowek.remove();
+  });
+}
+
+init();
+}
